d47421809e26f73822ff3d43caf11083cd582907
hyc4841 <dbscjf4841@naver.com>
2024-07-08 19:13:07 +0900

    first commit

---
+++ src/main/java/whang/travel/domain/member/Member.java
@@ -0,0 +1,16 @@
+package whang.travel.domain.member;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private Long id;
+
+    private String loginId;
+    private String name;
+    private String password;
+    private String email;
+
+    private String 
+}
