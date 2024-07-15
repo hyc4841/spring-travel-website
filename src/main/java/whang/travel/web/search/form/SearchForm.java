deb52d0b24e645de473f0efec159491d0d591352
hyc4841 <dbscjf4841@naver.com>
2024-07-15 21:33:52 +0900

    home update

---
+++ src/main/java/whang/travel/web/search/form/SearchForm.java
@@ -0,0 +1,11 @@
+package whang.travel.web.search.form;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class SearchForm {
+
+    @NotBlank
+    private String region;
+}
