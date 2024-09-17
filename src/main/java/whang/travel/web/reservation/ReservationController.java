42d85cd07b765e92abc8c9e9a99226f0549be62c
hyc4841 <dbscjf4841@naver.com>
2024-09-17 23:37:37 +0900

    에약 화면 업데이트

--- src/main/java/whang/travel/web/reservation/ReservationController.java
+++ src/main/java/whang/travel/web/reservation/ReservationController.java
@@ -3,6 +3,7 @@ package whang.travel.web.reservation;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,9 +23,10 @@ public class ReservationController {
     private final ReservationService reservationService;
     private final AccommodationService accommodationService;
 
+    // Api로 하려면 화면 이동은 프론트에서 하고 서버는 데이터만 준다. csr
     @PostMapping("/check/{roomId}")
-    public String reservationView(@RequestBody Reservation reservation,
-                                  Model model, HttpServletResponse response) {
+    public HttpEntity<Reservation> reservationView(@RequestBody Reservation reservation,
+                                      Model model, HttpServletResponse response) {
 
         log.info("넘어온 예약 정보={}", reservation);
         model.addAttribute("reservation", reservation);
@@ -32,7 +34,7 @@ public class ReservationController {
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_OK);
 
-        return "";
+        return ;
     }
 
     @PostMapping("")
