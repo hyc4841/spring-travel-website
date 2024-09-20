a67f35b59e78f16bda05ffb49ad8ad0e36da2413
hyc4841 <dbscjf4841@naver.com>
2024-09-20 14:28:34 +0900

    결재 이후 예약 내용 저장

--- src/main/java/whang/travel/web/reservation/ReservationController.java
+++ src/main/java/whang/travel/web/reservation/ReservationController.java
@@ -26,6 +26,7 @@ import whang.travel.domain.reservation.Reservation;
 import whang.travel.domain.reservation.mybatis.ReservationService;
 import whang.travel.web.reservation.form.ReservationApiResponse;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Slf4j
@@ -54,37 +55,42 @@ public class ReservationController {
     @GetMapping("/checkout/{roomId}")
     public String reservationView(@PathVariable Long roomId, @ModelAttribute("reservation") Reservation reservation,
                                   Model model, @AuthenticationPrincipal UserDetails user) {
-
-        if (user != null) {
+        if (user != null) { // 현재 로그인한 유저가 있으면. 로그인 유저의 정보 가져와서 번호 뿌려주기
             Optional<Member> member = memberRepository.findByLoginId(user.getUsername());
             reservation.setNumber(member.get().getNumber());
         }
 
         Room room = accommodationService.findRoomById(roomId);
+        Accommodation accommodation = accommodationService.findAccommoById(room.getAccommodation()).get();
+
+
+        model.addAttribute("accommodation", accommodation);
         model.addAttribute("room", room);
-        // 예약할 때 무슨 데이터가 넘어가야 할까?
-        // 예약자, 숙소 이름
 
         return "/reservation/addReservation";
     }
 
     @PostMapping("/checkout/{roomId}")
-    public ResponseEntity<String> addReservation(@RequestBody Reservation reservation, @AuthenticationPrincipal UserDetails user) {
-        String loginId = user.getUsername();
-        Long memberId = memberRepository.findIdByLoginId(loginId);
-        // 판매자 가져오기 => 숙소 id로 가져올 수 있다.
-        // 숙소 가져오기
-        // 예약 날짜
-        //
-        try {
+    public String addReservation(@PathVariable Long roomId, @ModelAttribute("reservation") Reservation reservation, @AuthenticationPrincipal UserDetails user) {
+        if (user != null) {
+            String loginId = user.getUsername();
+            Long memberId = memberRepository.findIdByLoginId(loginId);
             reservation.setMember(memberId);
-            Reservation save = reservationService.save(reservation);
-            log.info("결재 성공={}", save);
-            return ResponseEntity.ok().build();
-        } catch (RuntimeException e) {
-            log.info("오류 발생");
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+
+        Room room = accommodationService.findRoomById(roomId);
+        Accommodation accommodation = accommodationService.findAccommoById(room.getAccommodation()).get();
+
+        reservation.setSeller(accommodation.getSeller());
+        reservation.setAmount(room.getBasePrice());
+        reservation.setRoom(room.getRoomId());
+        reservation.setRDate(new Date());
+        reservation.setAccommodation(accommodation.getAccommodationId());
+
+        reservationService.save(reservation);
+
+        log.info("정상 작동, 넘어온 예약정보={}", reservation);
+        return "redirect:/home";
     }
 
     @PostMapping("/payment/validation/{imp_uid}")
@@ -96,7 +102,6 @@ public class ReservationController {
     }
 
     @GetMapping("/room/{roomId}")
-    @ResponseBody
     public ResponseEntity<Room> findRoom(@PathVariable Long roomId) {
         Room room = accommodationService.findRoomById(roomId);
         Long accommodationId = room.getAccommodation();
