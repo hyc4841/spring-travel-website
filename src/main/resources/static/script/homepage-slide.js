1f8ab6f373f19fa849ed71a32e6378e4e69a158c
hyc4841 <dbscjf4841@naver.com>
2024-10-11 23:56:43 +0900

    홈 페이지 수정

---
+++ src/main/resources/static/script/homepage-slide.js
@@ -0,0 +1,28 @@
+/*
+document.addEventListener("DOMContentLoaded", function() {
+
+    var mySwiper = new Swiper('.swiper-container', {
+        cssMode: true,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        observer: true,
+        observeParents: true,
+        spaceBetween: 2,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            1280: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+            },
+            720: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+            }
+        }
+    });
+
+});
+*/
